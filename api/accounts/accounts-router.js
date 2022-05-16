189b28d4e61367cdd2805724f93399d13826456e
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-16 16:01:56 -0700

    endpoints + error handling

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,49 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    res.json("get accounts")
+  } catch(err){
+    next({ status: 422, message: "danger danger"})
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+      res.json("get accounts by id")
+  } catch(err){
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    res.json("post accounts")
+  } catch(err){
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    res.json("update accounts")
+  } catch(err){
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try{
+    res.json("delete accounts")
+  } catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+ res.status(err.status || 500).json({
+   message: err.message
+ })
 })
 
 module.exports = router;
